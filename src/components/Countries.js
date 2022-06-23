e2535e10b7173d7b2febfcff6e42e1105bd54526
ondjoss <ghislain.chakam@gmail.com>
2022-06-23 11:48:53 +0200

    Création des composants logo, countries, navigation. Ajout de la librairie Axios.

---
+++ src/components/Countries.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+
+const Countries = () => {
+    return (
+        <div className='countries'>
+            <h1>Countries</h1>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Logo from '../components/Logo';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>À Propos</h1>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum fuga tempore quaerat eius blanditiis ea pariatur officiis eos perspiciatis? Magnam praesentium, accusamus nemo provident modi inventore eos voluptate blanditiis aliquid.
+            Nesciunt eaque sequi voluptatem harum nemo laborum eveniet tenetur, ex voluptate. Eius odio numquam dolore repellendus atque labore, quia optio reiciendis perspiciatis. Nesciunt numquam necessitatibus rerum, eligendi saepe excepturi non.
+            Labore tempora, exercitationem necessitatibus tenetur recusandae laudantium sint nobis delectus deserunt ea ullam quasi perferendis pariatur repudiandae aperiam ducimus maxime corrupti, voluptatibus corporis tempore neque. Repellendus labore aliquam minus ullam!
+            Eaque vero ipsa, doloremque error quia molestiae beatae maiores quo alias accusamus eos delectus fuga incidunt asperiores neque! Delectus praesentium ducimus illo non nulla inventore corporis nam aspernatur, vitae nostrum.
+            Explicabo rerum consectetur tempora esse voluptatum id omnis asperiores temporibus voluptatibus! Facilis, repudiandae id? Rem delectus eos quae voluptatem doloribus recusandae vitae culpa iste. Sunt totam dolorum commodi laborum obcaecati.
+            Nobis reprehenderit laudantium voluptas provident nulla sunt amet maiores aperiam, temporibus qui repellat consectetur! Recusandae magni possimus eos sapiente distinctio magnam nobis facilis. Hic voluptatem tempore, itaque ad quidem nulla!
+            Facere ipsam adipisci cumque, aspernatur minus excepturi fugiat. Laborum provident earum reprehenderit eius! Velit inventore impedit voluptates, ab labore eaque. Iure, recusandae fugiat! Numquam, maiores. Itaque accusantium nostrum omnis assumenda.
+            </p>
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum porro placeat similique suscipit vitae in assumenda, voluptatibus consequatur provident nesciunt, consectetur mollitia ipsum beatae corporis facere eos optio? Eos, hic.
+            Incidunt obcaecati temporibus impedit, nulla labore sapiente libero placeat! Voluptate labore illum fuga ex nemo eligendi sunt mollitia, saepe esse corporis accusamus delectus temporibus quo nesciunt veniam. Facere, odit laborum?
+            Deleniti, aspernatur cumque dolores modi fugiat id quasi. Consequuntur iure quae sint maiores delectus magnam laudantium quaerat reprehenderit, possimus eveniet doloribus necessitatibus dolor, facilis ut ab, soluta est! Cupiditate, pariatur?
+            Consequuntur, quam ad architecto illo autem facilis quaerat aliquam consequatur voluptatem! Reprehenderit fuga sint sunt deserunt corporis excepturi pariatur explicabo. Nemo exercitationem incidunt quis libero illum doloremque ad ipsum at?
+            At, vitae. Nesciunt ipsum consequatur quos molestiae ratione. Nostrum eius illum mollitia explicabo reiciendis consectetur adipisci ad in quaerat asperiores error esse provident, itaque non, quis deserunt, velit praesentium? Nobis.
+            </p>
+        </div>
+    );
+};
+
+export default About;
